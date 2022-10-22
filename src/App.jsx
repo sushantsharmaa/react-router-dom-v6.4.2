910a3814cd0bc22e034e599f02f4489083bb66f0
Sushant Sharma <sushant.x01@gmail.com>
2022-10-22 15:27:15 +0530

    Configuring Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
-import "./App.css";
+import Home from "./components/Home/Home";
+import About from "./components/About/About";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <h1>React Router Dom</h1>
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+    </Routes>
   );
 }
 
