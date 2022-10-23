63538dfc6acf222aad79d3b941cc7cc06456b75c
Sushant Sharma <sushant.x01@gmail.com>
2022-10-23 14:57:20 +0530

    Use Links to navigate from one page to another

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import Home from "./components/Home/Home";
 import About from "./components/About/About";
+import Navbar from "./components/Navbar/Navbar";
 import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About />} />
-    </Routes>
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+      </Routes>
+    </>
   );
 }
 
