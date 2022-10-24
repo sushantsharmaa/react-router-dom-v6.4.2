9f1ec0776423c7bf4f3593ef8a0849358a52f67a
Sushant Sharma <sushant.x01@gmail.com>
2022-10-24 07:55:55 +0530

    Nested Routes

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import About from "./components/About/About";
 import Navbar from "./components/Navbar/Navbar";
 import { Routes, Route } from "react-router-dom";
 import OrderSummary from "./components/OrderSummary/OrderSummary";
+import Products from "./components/Products/Products";
+import FeaturedProducts from "./components/Products/FeaturedProducts";
+import NewProducts from "./components/Products/NewProducts";
 import NoMatch from "./components/NoMatch/NoMatch";
 
 function App() {
@@ -13,6 +16,10 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="about" element={<About />} />
         <Route path="order-summary" element={<OrderSummary />}></Route>
+        <Route path="products" element={<Products />}>
+          <Route path="featured" element={<FeaturedProducts />} />
+          <Route path="new" element={<NewProducts />} />
+        </Route>
         <Route path="*" element={<NoMatch />} />
       </Routes>
     </>
